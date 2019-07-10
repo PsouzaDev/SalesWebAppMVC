f3097bced9719210b1f8fbcc00f583db698d6754
Pedro Cordeiro de Souza <p.cordeiro.de.souza@avanade.com>
2019-07-10 15:52:54 -0300

    First controller and Razor pages tests

--- SalesWebAppMVC/Controllers/HomeController.cs
+++ SalesWebAppMVC/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace SalesWebAppMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales web mvc app.";
 
             return View();
         }
