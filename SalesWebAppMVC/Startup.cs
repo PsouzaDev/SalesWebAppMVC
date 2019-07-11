8b9950346edfc083f1f14dc01d1a4b560eb76660
Pedro Cordeiro de Souza <p.cordeiro.de.souza@avanade.com>
2019-07-11 11:05:40 -0300

    MySQL adaptation and first migration

--- SalesWebAppMVC/Startup.cs
+++ SalesWebAppMVC/Startup.cs
@@ -37,7 +37,8 @@ namespace SalesWebAppMVC
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SalesWebAppMVCContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebAppMVCContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SalesWebAppMVCContext"), builder =>
+                    builder.MigrationsAssembly("SalesWebAppMVC")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
